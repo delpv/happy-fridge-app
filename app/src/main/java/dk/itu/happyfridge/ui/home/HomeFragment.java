00b93c5cca412ebc6b6dba92622c4c1c22321ffb
Catalina <catalinaelena08@yahoo.com>
2025-04-15 16:24:27 +0200

    update app with latest wip

---
+++ app/src/main/java/dk/itu/happyfridge/ui/home/HomeFragment.java
@@ -0,0 +1,37 @@
+package dk.itu.happyfridge.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import dk.itu.happyfridge.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        // Set the text dynamically using LiveData
+        homeViewModel.getText().observe(getViewLifecycleOwner(), binding.textHome::setText);
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
